c199deade9f4cd9ff93457a8a8c204f5c333c52d
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2017-02-04 14:50:06 +0400

    fix indexing after sort

--- src/utils/csv.js
+++ src/utils/csv.js
@@ -4,9 +4,10 @@ let fromCSV = data =>
     .split(/\n/)
     .map(p => p.split(','))
     .filter(Boolean)
-    .map((p, i) => ({ i, x: +p[0], y: +p[1] }))
+    .map(p => ({ x: +p[0], y: +p[1] }))
     .filter(p => Boolean(p.x) && Boolean(p.y))
     .sort((a, b) => a.x - b.x)
+    .map((p, i) => ({ ...p, i }))
 
 let toCSV = points =>
   points.map(point => `${point.x},${point.y}`).join('\n')
