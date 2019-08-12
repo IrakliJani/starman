ecd5c74dbb797c7c04cbf6a42dfc9b61104dbeae
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2019-08-12 12:42:38 +0400

    better output with rounding and precission

--- src/utils/csv.js
+++ src/utils/csv.js
@@ -4,13 +4,37 @@ export const fromCSV = data => {
     .split(/\n/)
     .map(line => line.split(','))
     .filter(Boolean)
-    .map(points => ({ x: Number(points[0]), y: Number(points[1]) }))
-    .filter(p => Boolean(p.x) && Boolean(p.y))
-    .map((p, i) => ({ ...p, i }))
+    .map(point => ({
+      x: Number.parseFloat(point[0]),
+      y: Number.parseFloat(point[1]),
+      xOrig: point[0],
+      yOrig: point[1],
+    }))
+    .filter(point => Boolean(point.x) && Boolean(point.y))
+    .map((point, i) => ({ ...point, i }))
 }
 
 export const toCSV = points => {
+  const maxPrecission = points.reduce((acc, point) => {
+    if (isFloat(Number(point.yOrig))) {
+      const precission = point.yOrig.split('.')[1].length
+      return precission > acc ? precission : acc
+    } else {
+      return acc
+    }
+  }, 0)
+
   return points
-    .map(point => `${point.x},${point.y}`)
+    .map(point => {
+      const yModified = maxPrecission > 0
+        ? point.y.toFixed(maxPrecission).replace(/0+$/, '')
+        : point.y
+
+      return `${point.xOrig},${yModified}`
+    })
     .join('\n')
+}
+
+function isFloat(n) {
+  return n % 1 !== 0
 }
